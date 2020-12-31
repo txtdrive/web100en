f37c9015ebbcb74482405e208ccf251e9d7f67e5
txtdrive <txtdrive@gmail.com>
2020-12-31 15:09:16 +0900

    maintenance

--- config/application.php
+++ config/application.php
@@ -97,7 +97,6 @@ Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 Config::define('DISALLOW_FILE_EDIT', true);
 // Disable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', true);
-Config::define(‘WP_MEMORY_LIMIT’, ’64M’);
 
 /**
  * Debugging Settings
